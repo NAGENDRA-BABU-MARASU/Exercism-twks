063d26b88b26cb34bc754a0092c3f28f7183df98
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-17 12:45:42 +0530

    completed Leap exercise

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,11 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if(year % 100 == 0){
+            return year % 400 == 0;
+        } else {
+            return year % 4 == 0;
+        }
+    }
+
+}
