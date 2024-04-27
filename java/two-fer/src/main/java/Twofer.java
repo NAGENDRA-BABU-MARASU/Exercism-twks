9cef2050f851dbfebb6d2f529a8854afa05b5b22
Nagendra <nagendrababu.marasu@gmail.com>
2024-04-27 09:44:07 +0530

    made changes to twofer

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,6 +1,5 @@
 public class Twofer {
     public String twofer(String name) {
-        boolean isNameNotGiven = name == null;
         String word = (name == null) ? "you" : name;
         return String.format("One for %s, one for me.", word);
     }
