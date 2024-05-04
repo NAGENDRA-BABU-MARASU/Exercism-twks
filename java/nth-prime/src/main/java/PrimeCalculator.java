023ff7f868edee9c6b32e687d180b3190f98d06f
Nagendra <nagendrababu.marasu@gmail.com>
2024-05-04 07:33:35 +0530

    feedback: optimised the prime number checker

--- java/nth-prime/src/main/java/PrimeCalculator.java
+++ java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,31 +1,30 @@
 class PrimeCalculator {
 
     int nth(int nth) {
-        if(nth == 0){
+        if (nth == 0) {
             throw new IllegalArgumentException("0th prime is undefined");
         }
         int primeNumberCount = 0;
         int currentNumber = 0;
-        while(primeNumberCount < nth) {
+        while (primeNumberCount < nth) {
             currentNumber += 1;
-            if(isPrime(currentNumber)) {
+            if (isPrime(currentNumber)) {
                 primeNumberCount += 1;
             }
         }
         return currentNumber;
     }
 
-    private boolean isPrime(int number){
-        if(number < 2){
+    private boolean isPrime(int number) {
+        if (number < 2) {
             return false;
         }
-        int maxDivisor = number / 2;
-        for(int divisor = 2; divisor <= maxDivisor; divisor++){
-            if(number % divisor == 0){
+
+        for (int divisor = 2; divisor * divisor <= number; divisor++) {
+            if (number % divisor == 0) {
                 return false;
             }
         }
         return true;
     }
-
 }
